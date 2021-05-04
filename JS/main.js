6f5dcdf7c3a7fe26df4740f55222ec14c59436f2
frezitopia <anri.axaladze@gmail.com>
2021-05-04 21:04:31 +0400

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,35 @@
 Circles.create({
-    id:           'circles-1',
-    value:        550.20,
-    maxValue:     615,
-    radius:       60,
-    width:        10,
-    duration:     1,
-    textClass:    'BTU-circlesTxt',
-    colors:       ['#9EBCA8', '#6C9B7C'],
+  id: 'circles-1',
+  value: 550.20,
+  maxValue: 615,
+  radius: 60,
+  width: 10,
+  duration: 1,
+  textClass: 'BTU-circlesTxt',
+  colors: ['#9EBCA8', '#6C9B7C'],
 });
 
 Circles.create({
-    id:           'circles-2',
-    value:        93.8,
-    maxValue:     110,
-    radius:       60,
-    width:        10,
-    duration:     1,
-    textClass:    'BTU-circlesTxt',
-    decimalClass:    'BTU-circlesTxt',
-    colors:       ['#E191BF', '#DE2891'],
+  id: 'circles-2',
+  value: 93.8,
+  maxValue: 110,
+  radius: 60,
+  width: 10,
+  duration: 1,
+  textClass: 'BTU-circlesTxt',
+  decimalClass: 'BTU-circlesTxt',
+  colors: ['#E191BF', '#DE2891'],
 });
 
 Circles.create({
-    id:           'circles-3',
-    value:        3.87,
-    maxValue:     4.5,
-    radius:       60,
-    width:        10,
-    duration:     1,
-    textClass:    'BTU-circlesTxt',
-    colors:       ['#A9E4E8', '#0DBECB'],
+  id: 'circles-3',
+  value: 3.87,
+  maxValue: 4.5,
+  radius: 60,
+  width: 10,
+  duration: 1,
+  textClass: 'BTU-circlesTxt',
+  colors: ['#A9E4E8', '#0DBECB'],
 });
 
 
@@ -48,11 +48,13 @@ jQuery("#carousel").owlCarousel({
     },
 
     600: {
-      items: 1
+      items: 2
     },
-
+    952: {
+      items: 2
+    },
     1024: {
-      items: 3
+      items: 2
     },
 
     1366: {
@@ -60,3 +62,14 @@ jQuery("#carousel").owlCarousel({
     }
   }
 });
+
+var sidebar = document.querySelector("#sidebar-wrapper");
+var btn = document.querySelector(".BTU-toggler");
+var drop = document.querySelector(".BTU-collapse");
+var w = drop.offsetHeight;
+
+btn.addEventListener("click", () => {
+    sidebar.style.transition = "all 0.5s";
+    // sidebar.style.marginTop = 98 + "px";
+    sidebar.classList.toggle("headMargin");
+})
